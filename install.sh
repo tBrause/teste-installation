6583cd5dd1921718eade8239dc5b95c179c4e478
tBrause <brause.torsten@gmail.com>
2023-03-19 22:09:49 +0100

    y

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-
-echo Installation: npm DEV-Tools
-
-# read -p "Domain: (wenn leer dann localhost)" domain
-
-# read -p "Port: " port
-
-# echo $domain":"$port
+#
+echo "Installation: npm DEV-Tools"
+#
+echo "lalal"
